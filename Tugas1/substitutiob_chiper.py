0245e798123de3513338421b2a770214211263fe
Muhammad Gandhi Fuat Saputra <gandhift@gmail.com>
2023-03-19 21:48:17 +0700

    tugas1

---
+++ Tugas1/substitutiob_chiper.py
@@ -0,0 +1,22 @@
+import string
+
+def substitution_cipher(key, plaintext):
+    ciphertext = ""
+
+    alphabet = string.ascii_lowercase
+    table = str.maketrans(alphabet, key)
+
+    for char in plaintext:
+        if not char.isalpha():
+            ciphertext += char
+            continue
+        if char.islower():
+            new_char = key[alphabet.index(char)]
+        else:
+            new_char = key[alphabet.index(char.lower())].upper()
+        ciphertext += new_char
+
+    return ciphertext
+
+ciphertext = substitution_cipher("phqgiumeaylnofdxjkrcvstzwb", "gandhi")
+print(ciphertext)
